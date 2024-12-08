8de73035308410d800cea2314a3c61f78af9bdaf
esteb <esteban.bajana@epn.edu.ec>
2024-12-08 14:47:40 -0500

    chore: add architecture react

--- pictionary-fronted/tailwind.config.js
+++ pictionary-fronted/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue01: "#123595",
+        blue02: "204AD0",
+        green01: "38C41C"
+      }
+    },
   },
   plugins: [],
 }
